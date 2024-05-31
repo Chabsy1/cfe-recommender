e20cb73a3faf8932ece9557c01c121be4a0e79c6
Chabsy1 <chukwu.sydney@lmu.edu.ng>
2024-05-31 09:00:56 +0100

    modify

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -70,7 +70,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware', # request.htmx
 ]
+
 
 ROOT_URLCONF = 'cfehome.urls'
 
